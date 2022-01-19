66aa078597bdfe074e266773641ddc56f8519fdd
mbolanoss <mbolanoss@unal.edu.co>
2022-01-18 21:03:52 -0500

    Added CrossOrigin annotation to ChatController

--- src/main/java/com/medirec/medirec/chat/controllers/ChatController.java
+++ src/main/java/com/medirec/medirec/chat/controllers/ChatController.java
@@ -19,9 +19,11 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@CrossOrigin
 @Controller
 public class ChatController {
     @Autowired
